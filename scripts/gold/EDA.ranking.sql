76eddb017f7fcaa1139dee4894d25f05bd32d5ab
Zahra Kazemi <zahra.kazemi1372@yahoo.com>
2025-10-12 01:31:00 -1200

    Update EDA.ranking.sql

--- scripts/gold/EDA.ranking.sql
+++ scripts/gold/EDA.ranking.sql
@@ -1,17 +1,63 @@
--- find tha min and max of order date
---how many of years are available
-SELECT
-	MIN(order_date) AS first_order_date,
-	MAX(order_date) AS last_order_date,
-	DATEDIFF(YEAR, MIN(order_date),MAX(order_date))  AS order_range_years
-FROM gold.fact_sales;
+-- Which 5 products generate the highest revenue?
+SELECT TOP 5
+	p.product_name,
+	SUM(f.sales_amount) AS revenue
+FROM gold.fact_sales f
+LEFT JOIN  gold.dim_product p
+ON  f.product_key= p.product_key   
+GROUP BY p.product_name
+ORDER BY revenue DESC;
+
+SELECT *
+FROM (
+	SELECT 
+		p.product_name,
+		SUM(f.sales_amount) AS revenue,
+		RANK() OVER(ORDER BY SUM(f.sales_amount) DESC) AS rank_products
+	FROM gold.fact_sales f
+	LEFT JOIN  gold.dim_product p
+	ON  f.product_key= p.product_key   
+	GROUP BY p.product_name
+	) t
+WHERE rank_products<=5;
+
+-- What are the 5 worst-performing products in terms of sales?
+SELECT TOP 5
+	p.product_name,
+	SUM(f.sales_amount) AS revenue
+FROM gold.fact_sales f
+LEFT JOIN  gold.dim_product p
+ON  f.product_key= p.product_key   
+GROUP BY p.product_name
+ORDER BY revenue ASC;
+
+-- Find the top 10 customers who have generated the highest revenue
+SELECT TOP 10
+	c.customer_key,
+	c.first_name,
+	c.last_name,
+	SUM(f.sales_amount) AS sales_amount
+FROM gold.fact_sales f
+LEFT JOIN  gold.dim_customer c
+ON  f.customer_key= c.customer_key   
+GROUP BY 
+	c.customer_key,
+	c.first_name,
+	c.last_name
+ORDER BY sales_amount DESC;
 
 
-
---find the youngest and oldest customers
-SELECT
-	MIN(birthdate) AS oldest,
-	DATEDIFF(YEAR, MIN(birthdate), GETDATE()),
-	MAX(birthdate) AS youngest,
-	DATEDIFF(YEAR, MAX(birthdate), GETDATE())
-FROM gold.dim_customer;
+-- The 3 customers with the fewest orders placed
+SELECT TOP 3
+	c.customer_key,
+	c.first_name,
+	c.last_name,
+	COUNT(DISTINCT order_number) AS total_order
+FROM gold.fact_sales f
+LEFT JOIN  gold.dim_customer c
+ON  f.customer_key= c.customer_key   
+GROUP BY 
+	c.customer_key,
+	c.first_name,
+	c.last_name
+ORDER BY total_order ASC;
